8bc5e24ccebd6197e2f5006afdae2dff5422eacd
Buyole Isacko <isackobuyole@gmail.com>
2022-10-17 20:47:37 +0300

    registered the touch event of the user

--- app/src/main/java/com/example/paintapplication/PaintView.kt
+++ app/src/main/java/com/example/paintapplication/PaintView.kt
@@ -2,28 +2,72 @@ package com.example.paintapplication
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import com.example.paintapplication.MainActivity.Companion.paintBrush
+import com.example.paintapplication.MainActivity.Companion.path
 
-class PaintView :   View{
-    companion object{
+class PaintView : View {
+
+    var params: ViewGroup.LayoutParams? = null
+
+    companion object {
         var pathList = ArrayList<Path>()
         var colorList = ArrayList<Int>()
         var currentBrush = Color.BLACK
     }
-    constructor(context: Context) : this(context, null){
 
-    }
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
-
-    }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    constructor(context: Context) : this(context, null) {
+        init()
     }
 
-    private fun init(){
-        paintBrush
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        paintBrush.isAntiAlias = true
+        paintBrush.color = currentBrush
+        paintBrush.style = Paint.Style.STROKE
+        paintBrush.strokeJoin = Paint.Join.ROUND
+        paintBrush.strokeWidth = 8f
+
+        params = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        override fun onTouchEvent(event: MotionEvent?): Boolean {
+            var x = event.x
+            var y = event.y
+
+            when(event.action){
+                MotionEvent.ACTION_DOWN ->{
+                    path.moveTo(x,y)
+                    return true
+                }
+                MotionEvent.ACTION_MOVE ->{
+                    path.lineTo(x,y)
+                    pathList.add(path)
+                    colorList.add(currentBrush)
+                }
+                else -> return false
+            }
+            postInvalidate()
+            return false
+        }
     }
 
 }
