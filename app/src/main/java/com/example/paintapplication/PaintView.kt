a69a84bd6ec5e3a7e0b51ff9da18fd5eaad5d2d1
Buyole Isacko <isackobuyole@gmail.com>
2022-10-17 20:26:51 +0300

    Styled paint brush

--- app/src/main/java/com/example/paintapplication/PaintView.kt
+++ app/src/main/java/com/example/paintapplication/PaintView.kt
@@ -1,4 +1,29 @@
 package com.example.paintapplication
 
-class PaintView {
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import com.example.paintapplication.MainActivity.Companion.paintBrush
+
+class PaintView :   View{
+    companion object{
+        var pathList = ArrayList<Path>()
+        var colorList = ArrayList<Int>()
+        var currentBrush = Color.BLACK
+    }
+    constructor(context: Context) : this(context, null){
+
+    }
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
+
+    }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    }
+
+    private fun init(){
+        paintBrush
+    }
+
 }
